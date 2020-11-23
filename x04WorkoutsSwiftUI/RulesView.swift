ce2be67890a236623263cbeb238b9dc01b930bc1
Whoopinstick <k.m.paul68@gmail.com>
2020-11-23 18:22:23 -0500

    Update rules sheet

--- x04WorkoutsSwiftUI/RulesView.swift
+++ x04WorkoutsSwiftUI/RulesView.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct RulesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center) {
+            Text("Rules")
+                .font(.title)
+                .fontWeight(.bold)
+            
+            Spacer().frame(height: 20)
+            
+            Text("The value of each card represents the number of excercise you do.")
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            
+            Spacer().frame(height: 20)
+            
+            Text("J = 11, Q = 12, K = 13, A = 14\n\n♠️ = Push-ups\n\n❤️ = Pull-ups\n\n♣️ = Burpees\n\n♦️ = Jumping Jacks")
+        }
     }
 }
 
