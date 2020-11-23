161334236721cc35e37c88be9f9e156dc3542488
Whoopinstick <k.m.paul68@gmail.com>
2020-11-23 15:59:37 -0500

    add assets and initial UI setup

--- x04WorkoutsSwiftUI/ContentView.swift
+++ x04WorkoutsSwiftUI/ContentView.swift
@@ -9,8 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Image("AS")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 250, height: 350, alignment: .center)
+            
+            Spacer().frame(height: 20.0)
+            
+            Button("Stop!") {}
+                .frame(width: 250, height: 50, alignment: .center)
+                .foregroundColor(.white)
+                .background(Color.red)
+                .cornerRadius(8.0)
+            
+            Spacer().frame(height: 20.0)
+            
+            HStack {
+                Button("Restart") {}
+                    .frame(width: 115, height: 50, alignment: .center)
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(8.0)
+                
+                Spacer().frame(width: 20)
+                
+                Button("Rules") {}
+                    .frame(width: 115, height: 50, alignment: .center)
+                    .foregroundColor(.white)
+                    .background(Color.green)
+                    .cornerRadius(8.0)
+            }
+        }
     }
 }
 
