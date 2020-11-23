0bb3a8f0d5dd932facd5a313f8981e9c9e655b38
Whoopinstick <k.m.paul68@gmail.com>
2020-11-23 15:45:17 -0500

    Initial Commit

---
+++ x04WorkoutsSwiftUI/x04WorkoutsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  x04WorkoutsSwiftUIApp.swift
+//  x04WorkoutsSwiftUI
+//
+//  Created by Kevin Paul on 11/23/20.
+//
+
+import SwiftUI
+
+@main
+struct x04WorkoutsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
